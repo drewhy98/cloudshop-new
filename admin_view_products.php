883aacc954764b1645cef7e0095fae184da85539
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2025-12-01 13:57:34 +0000

    Update admin_view_products.php

--- admin_view_products.php
+++ admin_view_products.php
@@ -1,6 +1,6 @@
 <?php 
 session_start(); 
-require_once "db_write.php"; // admin needs write permission
+require_once "dbconnect.php";// admin needs write permission
 
 // Ensure admin is logged in
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
