cf06a8344d60562c15f9879f594fd127f4a25971
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2025-12-01 14:10:41 +0000

    Update wishlist.php

--- wishlist.php
+++ wishlist.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "db_read.php"; // read-only replica
+require_once "dbconnect.php"; // read-only replica
 
 // Ensure user is logged in
 if (!isset($_SESSION['user_id'])) {
