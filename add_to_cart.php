e0311b8afc734fab85f254d3305c548faa53d51a
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2025-12-01 13:52:45 +0000

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "db_write.php"; // <-- for write operations
+require_once "dbconnect.php"; // <-- for all operations
 
 // Ensure user is logged in
 if (!isset($_SESSION['user_id'])) {
